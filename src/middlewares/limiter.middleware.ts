1a4f93925579cc72b660f488c35cc8fa2f227425
Konrad <33784221+theinfinit@users.noreply.github.com>
2025-11-23 14:23:00 +0100

    (limiter.middleware.ts) Fix hono c.var typescript error
    Source: https://hono.dev/docs/api/context#var

--- src/middlewares/limiter.middleware.ts
+++ src/middlewares/limiter.middleware.ts
@@ -10,12 +10,12 @@ export function limiter({
   minutes: number;
   key?: string;
 }) {
-  return rateLimiter({
+  return rateLimiter<HonoTypes>({
     windowMs: minutes * 60 * 1000, // every x minutes
     limit, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
     standardHeaders: "draft-6", // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
     keyGenerator: (c) => {
-      const vars = c.var as HonoTypes["Variables"];
+      const vars = c.var;
       const clientKey = vars.userId || c.req.header("x-forwarded-for");
       const pathKey = key || c.req.routePath;
       return `${clientKey}_${pathKey}`;
